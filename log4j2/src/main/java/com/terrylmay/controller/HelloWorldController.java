36be94d0000b6f413fddf9c2043f95f455d91855
terryKing1992 <terryllmay.gmail.com>
2018-08-27 23:49:50 +0800

    添加两个工程, 对比日志写入的性能

---
+++ log4j2/src/main/java/com/terrylmay/controller/HelloWorldController.java
@@ -0,0 +1,34 @@
+package com.terrylmay.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class HelloWorldController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldController.class);
+
+    public static void logging() {
+        LOGGER.info("HelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorldHelloWorld");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Long startTime = System.currentTimeMillis();
+        int threadCount = 2;
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        for (int index = 0; index < threadCount; index++) {
+            new Thread(new Runnable() {
+                public void run() {
+                    for (int i = 0; i < 2000000; i++) {
+                        logging();
+                    }
+                    countDownLatch.countDown();
+                }
+            }).start();
+        }
+        countDownLatch.await();
+        Long endTime = System.currentTimeMillis();
+        LOGGER.info("total cost time is:{}", (endTime - startTime));
+    }
+}
